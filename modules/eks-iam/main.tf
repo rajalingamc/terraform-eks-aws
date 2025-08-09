43fb2bf7f6b8ef226683fe1cf1de36748e414887
rajalingamc <109029502+rajalingamc@users.noreply.github.com>
2025-08-09 09:00:19 +0530

    Create main.tf

---
+++ modules/eks-iam/main.tf
@@ -0,0 +1,113 @@
+resource "aws_iam_openid_connect_provider" "eks_oidc" {
+
+  url = var.eks_cluster_oidc_issuer_in
+
+  client_id_list = ["sts.amazonaws.com"]
+
+  thumbprint_list = [var.eks_cluster_oidc_thumbprint_in]
+}
+
+
+
+resource "aws_iam_role" "eks_cluster" {
+  name = "eks_cluster_role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "eks.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster.id
+}
+
+resource "aws_iam_role" "eks_node" {
+  name = "eks_node_role"
+
+  assume_role_policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": "ec2.amazonaws.com"
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+POLICY
+}
+
+resource "aws_iam_role" "eks_ebs_csi_driver_role" {
+  name               = "AmazonEKS_EBS_CSI_DriverRole"
+  path               = "/"
+  max_session_duration = 3600
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect = "Allow"
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.eks_oidc.arn
+        }
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Condition = {
+          StringEquals = {
+            "${replace(var.eks_cluster_oidc_issuer_in, "https://", "")}:aud" = "sts.amazonaws.com"
+          }
+        }
+      }
+    ]
+  })
+
+  tags = {
+    Name = "AmazonEKS_EBS_CSI_DriverRole"
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "eks_ebs_csi_driver_role_attach" {
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+  role       = aws_iam_role.eks_ebs_csi_driver_role.id
+}
+
+
+
+resource "aws_iam_role_policy_attachment" "AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.eks_node.id
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.eks_node.id
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.eks_node.id
+}
+
+resource "aws_iam_role_policy_attachment" "AmazonSSMFullAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
+  role       = aws_iam_role.eks_node.id
+}
+
+
+
+
+
